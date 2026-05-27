cbd2658eae3b2e319215c307d1732f67d1a7bb4b
Sean Baxter <developer@misfitis.land>
2026-05-27 13:10:57 -0600

    Simplify homepage content flow

--- shared-nav.js
+++ shared-nav.js
@@ -18,10 +18,10 @@
           '<a href="' + pathPrefix + 'key-terms/">Key Terms</a>' +
           '<a href="' + pathPrefix + 'learning/">The Lab</a>' +
           '<a href="' + pathPrefix + 'standard/">The Standard</a>' +
-          '<a href="' + pathPrefix + 'grinnell-tracker/">Calculator</a>' +
           '<div class="nav-resource-group">' +
             '<button class="nav-resource-toggle" type="button" aria-expanded="false" aria-controls="other-resources-menu">Other Resources<span aria-hidden="true">v</span></button>' +
             '<div class="nav-resource-menu" id="other-resources-menu">' +
+              '<a href="' + pathPrefix + 'grinnell-tracker/">Grinnell Calculator</a>' +
               '<a href="https://wakeupandproduce.zohocs.com/" target="_blank" rel="noopener">The Producer Community</a>' +
               '<a href="https://www.youtube.com/playlist?list=PLpHNJgi9uGrlzcQB8ElVVGjYvUo1mnVDY" target="_blank" rel="noopener">Video Library</a>' +
               '<a href="https://pocketcoach.training" target="_blank" rel="noopener">Pocket Coach</a>' +
